eccb0c25e3cc4276b18feb507b561f555e9ec98d
pantbros <132388554+pantbros@users.noreply.github.com>
2025-06-04 19:43:14 +0000

    fixed sort issues..

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,22 @@
 import altImg from '../modules/altImg.js';
 let contentWrapper = document.getElementById('root');
-let selectTag = document.getElementById('select');
-console.log(selectTag.children);
-
-const students = [
-    { id: 1, imgUrl:`assets/img/img2.png`, stName: 'Amit Sharma', Marks: 90 , Grade: 85 },
-    { id: 2, imgUrl:`assets/img/img2.png`, stName: 'Riyash Sharma', Marks: 92, Grade: 85 },
-    { id: 3, imgUrl:`assets/img/img2.png`, stName: 'Amit Joshi', Marks: 76, Grade: 85 },
-    { id: 4, imgUrl:`assets/img/img2.png`, stName: 'Neha Verma', Marks: 59, Grade: 85 },
-    { id: 5, imgUrl:`assets/img/img2.png`, stName: 'Raj Singh', Marks: 48, Grade: 85 },
-    { id: 6, imgUrl:`assets/img/img2.png`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
-    { id: 7, imgUrl:`assets/img/img2.png`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
-    { id: 8, imgUrl:`assets/img/img2.png`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
-    { id: 9, imgUrl:`assets/img/img2.png`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
-    { id: 10, imgUrl:`assets/img/img2.png`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
-    { id: 11, imgUrl:`assets/img/img2.png`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
-    { id: 12, imgUrl:`assets/img/img2.png`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
-    { id: 13, imgUrl:`assets/img/img2.png`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
-];
-
-
 
 let marks = [];
+const students = [
+    { id: 1, imgUrl:`assets/img/img2.jpg`, stName: 'Amit Sharma', Marks: 90 , Grade: 85 },
+    { id: 2, imgUrl:`assets/img/img2.jpg`, stName: 'Riyash Sharma', Marks: 92, Grade: 85 },
+    { id: 3, imgUrl:`assets/img/img2.jpg`, stName: 'Amit Joshi', Marks: 76, Grade: 85 },
+    { id: 4, imgUrl:`assets/img/img2.jpg`, stName: 'Neha Verma', Marks: 59, Grade: 85 },
+    { id: 5, imgUrl:`assets/img/img2.jpg`, stName: 'Raj Singh', Marks: 48, Grade: 85 },
+    { id: 6, imgUrl:`assets/img/img2.jpg`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
+    { id: 7, imgUrl:`assets/img/img2.jpg`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
+    { id: 8, imgUrl:`assets/img/img2.jpg`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
+    { id: 9, imgUrl:`assets/img/img2.jpg`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
+    { id: 10, imgUrl:`assets/img/img2.jpg`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
+    { id: 11, imgUrl:`assets/img/img2.jpg`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
+    { id: 12, imgUrl:`assets/img/img2.jpg`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
+    { id: 13, imgUrl:`assets/img/img2.jpg`, stName: 'Kunal Mehra', Marks: 48, Grade: 85 },
+];
 students.forEach(function(student){
     if(student.Marks < 90){
         student.Grade = 'B+'
@@ -44,28 +39,34 @@ function cardstemp(imgurl, name,rollNo, marks, grade){
                             <h4>Grade : ${grade ? grade : 'E+'}</h4>
                         </div>`;
     contentWrapper.appendChild(element);
-    return imgurl, name, marks, grade;
-}
-function test(){
-    return 1;
-}
-function sortbymarks(){
-    return 0;
 }
 
-// sortbymarks();
-// test();
+document.getElementById('select').addEventListener('change', function(){
+    let selectValue = this.value;
+    contentWrapper.innerHTML = '';
+    let listToRender = [];
+    if(selectValue == "marks"){
+        listToRender = students.slice().sort((a,b)=> a.Marks -  b.Marks);
+    }else{
+        listToRender = students;
+    }
+    for(let i = 0; students.length > i; i++){
+        cardstemp(
+            listToRender[i].imgUrl,
+            listToRender[i].stName,
+            listToRender[i].id,
+            listToRender[i].Marks,
+            listToRender[i].Grade
+        )}
+    });
 
-if(test() == 5){
+
     for(let i = 0; students.length > i; i++){
-        cardstemp(students[i].imgUrl,students[i].stName,students[i].id,students[i].Marks,students[i].Grade);
-    }
-}else if(sortbymarks() == 5){
-    students = students.slice().sort((a,b)=> a.Marks -  b.Marks);
-    console.log(students);
-    
-    for(let i = 0; students.length > i; i++){
-        cardstemp(students[i].imgUrl,students[i].stName,students[i].id,students[i].Marks,students[i].Grade);
-    }
-}
+        cardstemp(
+            students[i].imgUrl,
+            students[i].stName,
+            students[i].id,
+            students[i].Marks,
+            students[i].Grade
+        )};
 altImg;
